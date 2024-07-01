d51b47b063c7aea8199a0ac0d3300315fb054803
VaIbHaVPiSe <vaibhavlspise2001@gmail.com>
2024-07-02 00:29:34 +0530

    Pattern6

---
+++ trickyQ/pattern6.c
@@ -0,0 +1,17 @@
+/* Read input from STDIN. Print your output to STDOUT*/
+#include<stdio.h>
+int main(int argc, char *a[])
+{
+	int num;
+	scanf("%i",&num);
+	
+	for(int i =0; i<num ;i++){
+		int k =65;
+		for(int j = 0;j<num;j++){
+			printf("%c ",k);
+            k++;
+		}
+		k++;
+		printf("\n");
+	}
+}
