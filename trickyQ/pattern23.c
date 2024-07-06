efceae2d924d191974c5f5b9d81c5ced1ce398ea
VaIbHaVPiSe <vaibhavlspise2001@gmail.com>
2024-07-06 23:35:08 +0530

    Pattern 23

---
+++ trickyQ/pattern23.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%i",&num);
+    int k=64+num;
+    for(int i=num;i>0;i--){
+        
+        for(int j=num;j>num-i;j--){
+            printf("%c",k-(num-j));
+        }
+        
+    printf("\n");
+    }
+}
