275408d3a1bd79df3f1ba9169dd8a2f9d28cfd1b
VaIbHaVPiSe <vaibhavlspise2001@gmail.com>
2024-07-12 15:34:40 +0530

    Pattern47

---
+++ trickyQ/pattern47.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num;
+    scanf("%i",&num);
+    for(int i=num;i>0;i--){
+        for(int j=1;j<=2*num ;j++){
+            if(j<num-i+1 || j>num+i-1){
+                printf(" ");
+            }else
+            printf("*");
+        }
+        printf("\n");
+    }
+}
